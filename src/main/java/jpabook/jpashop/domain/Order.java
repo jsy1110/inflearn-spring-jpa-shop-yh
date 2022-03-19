06aaeef587944a148b7713468189f1947cc0f1e5
foreverwin_zzi <jsy11101@gmail.com>
2022-03-20 00:34:15 +0900

    domain setting

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders") // 적어주지 않으면 관례로 table name이 order가 되는데 order by 의 SQL 구문때문에 orders 로 테이블 name을 지정
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
